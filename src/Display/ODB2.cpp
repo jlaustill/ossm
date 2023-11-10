2a3ae35d42c7499e2027d92750ecb75fbd8d8d36
Joshua Lawrence Austill <jlaustill@gmail.com>
2023-11-10 04:04:27 -0700

    does this table work

--- src/Display/ODB2.cpp
+++ src/Display/ODB2.cpp
@@ -12,7 +12,6 @@
 
 FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can1;
 
-
 unsigned long runtime;
 double boost;
 double oilPres;
@@ -61,10 +60,10 @@ void OBD2::sendData(const CAN_message_t &msg) {
         Serial.println("ODB2 pids 1-32 requested from " + (String)msg.id);
         Can1.write(supportedPidsOneToThirtyTwoResponse);
         break;
-      //   case 5:
-      //     waterTempResponse.buf[3] = OBD2::appData->coolantTemp;
-      //     Can1.write(waterTempResponse);
-      //     break;
+      case 5:
+        waterTempResponse.buf[3] = OBD2::appData->coolantTemperature + 40;
+        Can1.write(waterTempResponse);
+        break;
       case 15:
         ambientTemperatureResponse.buf[3] =
             OBD2::appData->ambientTemperature + 40;
@@ -88,23 +87,23 @@ void OBD2::sendData(const CAN_message_t &msg) {
         Serial.println("ODB2 pids 65-96 requested");
         Can1.write(supportedPidsSixtyFiveToNinetySixResponse);
         break;
-        //   case 92:
-        //     oilTempResponse.buf[3] = OBD2::appData->oilTempC + 40;
-        //     Can1.write(oilTempResponse);
-        //     break;
+      case 92:
+        oilTempResponse.buf[3] = OBD2::appData->oilTemperature + 40;
+        Can1.write(oilTempResponse);
+        break;
       case 96:
         Serial.println("ODB2 pids 97-128 requested");
         Can1.write(supportedPidsNinetySevenToOneHundredTwentyEightResponse);
         break;
-        //   case 112:
-        //     boost = OBD2::appData->boost;
-        //     boost *= 1.003;
-        //     boost *= 220;
-        //     boost -= 10;
-        //     boostResponse.buf[6] = boost < 0 ? 0 : highByte((long)boost);
-        //     boostResponse.buf[7] = boost < 0 ? 0 : lowByte((long)boost);
-        //     Can1.write(boostResponse);
-        //     break;
+      case 112:
+        boost = OBD2::appData->boostPressure;
+        boost *= 1.003;
+        boost *= 220;
+        boost -= 10;
+        boostResponse.buf[6] = boost < 0 ? 0 : highByte((long)boost);
+        boostResponse.buf[7] = boost < 0 ? 0 : lowByte((long)boost);
+        Can1.write(boostResponse);
+        break;
       case 128:
         Serial.println("ODB2 pids 129-160 requested");
         Can1.write(
@@ -130,52 +129,46 @@ void OBD2::sendData(const CAN_message_t &msg) {
         osAmbientConditionsResponse.buf[6] = lowByte((long)ambientAirTemp);
 
         Can1.write(osAmbientConditionsResponse);
-        // Serial.println("ODB2 ambient barometric pressure requested, sending "
-        // + (String)ambientAirPressure + " high byte: " +
-        // (String)osAmbientConditionsResponse.buf[3] + " low byte: " +
-        // (String)osAmbientConditionsResponse.buf[4]);
         break;
-        //   case 253:
-        //     oilPres = OBD2::appData->oilPressureInPsi;
-        //     oilPres *= 6.895;
-        //     oilPres *= 10;
-        //     oilResponse.buf[3] = highByte((long)oilPres);
-        //     oilResponse.buf[4] = lowByte((long)oilPres);
-        //     oilTemp = OBD2::appData->oilTempC;
-        //     oilTemp *= 100;
-        //     oilTemp += 32767;
-        //     oilResponse.buf[5] = highByte((long)oilTemp);
-        //     oilResponse.buf[6] = lowByte((long)oilTemp);
-        //     Can1.write(oilResponse);
-        //     break;
-        //   case 254:
-        //     // Serial.println("ODB2 254 requested");
-        //     manifoldPres = OBD2::appData->boost;
-        //     manifoldTemp = OBD2::appData->manifoldTempC;
-        //     manifoldTemp *= 100;
-        //     manifoldTemp += 32767;
-        //     manifoldResponse.buf[3] = highByte((long)manifoldTemp);
-        //     manifoldResponse.buf[4] = lowByte((long)manifoldTemp);
-        //     manifoldPres *= 6.895;
-        //     manifoldPres *= 10;
-        //     manifoldResponse.buf[5] = highByte((long)manifoldPres);
-        //     manifoldResponse.buf[6] = lowByte((long)manifoldPres);
-        //     Can1.write(manifoldResponse);
-        //     break;
-        //   case 255:
-        //     // Serial.println("ODB2 255 requested");
-        //     transPres = OBD2::appData->transmissionPressure;
-        //     transTemp = OBD2::appData->transmissionTempC;
-        //     transPres *= 6.895;
-        //     transPres *= 10;
-        //     transResponse.buf[5] = highByte((long)transPres);
-        //     transResponse.buf[6] = lowByte((long)transPres);
-        //     transTemp *= 100;
-        //     transTemp -= 32767;
-        //     transResponse.buf[3] = highByte((long)transTemp);
-        //     transResponse.buf[4] = lowByte((long)transTemp);
-        //     Can1.write(transResponse);
-        //     break;
+      case 253:
+        oilPres = OBD2::appData->oilPressure;
+        oilPres *= 6.895;
+        oilPres *= 10;
+        oilResponse.buf[3] = highByte((long)oilPres);
+        oilResponse.buf[4] = lowByte((long)oilPres);
+        oilTemp = OBD2::appData->oilTemperature;
+        oilTemp *= 100;
+        oilTemp += 32767;
+        oilResponse.buf[5] = highByte((long)oilTemp);
+        oilResponse.buf[6] = lowByte((long)oilTemp);
+        Can1.write(oilResponse);
+        break;
+      case 254:
+        manifoldPres = OBD2::appData->boostPressure;
+        manifoldTemp = OBD2::appData->boostTemperature;
+        manifoldTemp *= 100;
+        manifoldTemp += 32767;
+        manifoldResponse.buf[3] = highByte((long)manifoldTemp);
+        manifoldResponse.buf[4] = lowByte((long)manifoldTemp);
+        manifoldPres *= 6.895;
+        manifoldPres *= 10;
+        manifoldResponse.buf[5] = highByte((long)manifoldPres);
+        manifoldResponse.buf[6] = lowByte((long)manifoldPres);
+        Can1.write(manifoldResponse);
+        break;
+      case 255:
+        transPres = OBD2::appData->transmissionPressure;
+        transTemp = OBD2::appData->transmissionTemperature;
+        transPres *= 6.895;
+        transPres *= 10;
+        transResponse.buf[5] = highByte((long)transPres);
+        transResponse.buf[6] = lowByte((long)transPres);
+        transTemp *= 100;
+        transTemp -= 32767;
+        transResponse.buf[3] = highByte((long)transTemp);
+        transResponse.buf[4] = lowByte((long)transTemp);
+        Can1.write(transResponse);
+        break;
       default:
         // Serial.println("ODB2 unknown request ID: " + (String)msg.id + " " +
         //                (String)msg.buf[0] + " " + (String)msg.buf[1] + " " +
