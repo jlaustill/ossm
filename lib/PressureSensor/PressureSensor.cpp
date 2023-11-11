0bbdbbfaa83aa3247acdda20869658eb6e8ccbe1
Joshua Lawrence Austill <jlaustill@gmail.com>
2023-11-11 09:33:01 -0700

    only positive pressures lol

--- lib/PressureSensor/PressureSensor.cpp
+++ lib/PressureSensor/PressureSensor.cpp
@@ -3,38 +3,48 @@
 //
 
 #include "PressureSensor.h"
+
 #include <Arduino.h>
 
 void PressureSensor::updateSensor() {
-    this->rawValue = ads.computeVolts(ads.readADC_SingleEnded(ChannelId));
+  this->rawValue = ads.computeVolts(ads.readADC_SingleEnded(ChannelId));
 }
 
 double showDecimals(const double& x, const int& numDecimals) {
-    int y=x;
-    double z=x-y;
-    double m=pow(10,numDecimals);
-    double q=z*m;
-    double r=round(q);
+  int y = x;
+  double z = x - y;
+  double m = pow(10, numDecimals);
+  double q = z * m;
+  double r = round(q);
 
-    return static_cast<double>(y)+(1.0/m)*r;
+  return static_cast<double>(y) + (1.0 / m) * r;
+}
+
+double onlyPositive(const double& x) {
+  if (x < 0) {
+    return 0.0f;
+  }
+
+  return x;
 }
 
 float PressureSensor::getPressureInPsi() {
-    if (!initialized) {
-        return 0.0f;
-    }
+  if (!initialized) {
+    return 0.0f;
+  }
 
-    // Serial.println("RawValue->" + (String)this->rawValue);
+  // Serial.println("RawValue->" + (String)this->rawValue);
 
-    this->updateSensor();
-    float zeroVoltage = .32f;
-    float maxVoltage = 3.0f;
-    float returnValue = showDecimals(map(this->rawValue, zeroVoltage, maxVoltage, 0.0f, this->PsiMax), 1);
-    return returnValue;
+  this->updateSensor();
+  float zeroVoltage = .32f;
+  float maxVoltage = 3.0f;
+  float returnValue = onlyPositive(showDecimals(
+      map(this->rawValue, zeroVoltage, maxVoltage, 0.0f, this->PsiMax), 1));
+  return returnValue;
 }
 
 float PressureSensor::getPressureInkPa() {
-    float pressureInPsi = this->getPressureInPsi();
-    float pressureInkPa = pressureInPsi * 6.894757f;
-    return pressureInkPa;
+  float pressureInPsi = this->getPressureInPsi();
+  float pressureInkPa = pressureInPsi * 6.894757f;
+  return pressureInkPa;
 }
