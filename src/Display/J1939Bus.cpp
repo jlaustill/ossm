d78bee30cd60cde4c055cdeb38c344f5bd217162
Joshua Austill <jlaustill@gmail.com>
2024-11-27 22:04:54 -0700

    added configuration.h

--- src/Display/J1939Bus.cpp
+++ src/Display/J1939Bus.cpp
@@ -56,10 +56,14 @@ void J1939Bus::sendPgn65129(float engineIntakeManifold1AirTemperatureC,
   intakeTempOffset /= 0.03125;
   float coolantTempOffset = engineCoolantTemperatureC + 273.0;
   coolantTempOffset /= 0.03125;
+#if defined(SPN_1363_INTAKE_MANIFOLD_1_AIR_TEMPERATURE)
   msg.buf[0] = highByte(static_cast<uint16_t>(intakeTempOffset));   // 1636
   msg.buf[1] = lowByte(static_cast<uint16_t>(intakeTempOffset));    // 1636
+#endif
+#if defined(SPN_110_ENGINE_COOLANT_TEMPERATURE)
   msg.buf[2] = highByte(static_cast<uint16_t>(coolantTempOffset));  // 1637
   msg.buf[3] = lowByte(static_cast<uint16_t>(coolantTempOffset));   // 1637
+#endif
   msg.buf[4] = 255;
   msg.buf[5] = 255;
   msg.buf[6] = 255;
@@ -77,14 +81,18 @@ void J1939Bus::sendPgn65164() {
   CAN_message_t msg;
   msg.flags.extended = 1;
   msg.id = message.canId;
+#if defined(SPN_441_ENGINE_BAY_TEMPERATURE)
   msg.buf[0] =
       static_cast<uint8_t>(appData->engineBayTemperatureC + 40);  // 441
+#endif
   msg.buf[1] = 255;
   msg.buf[2] = 255;
   msg.buf[3] = 255;
   msg.buf[4] = 255;
   msg.buf[5] = 255;
+#if defined(SPN_354_RELATIVE_HUMIDITY)
   msg.buf[6] = static_cast<uint8_t>(appData->humidity / .4);  // 354
+#endif
   msg.buf[7] = 255;
 
   Can1.write(msg);
@@ -101,12 +109,18 @@ void J1939Bus::sendPgn65189(float engineIntakeManifold2TemperatureC,
   CAN_message_t msg;
   msg.flags.extended = 1;
   msg.id = message.canId;
+#if defined(SPN_1131_INTAKE_MANIFOLD_2_AIR_TEMPERATURE)
   msg.buf[0] =
       static_cast<uint8_t>(engineIntakeManifold2TemperatureC + 40);  // 1131
+#endif
+#if defined(SPN_1132_INTAKE_MANIFOLD_3_AIR_TEMPERATURE)
   msg.buf[1] =
       static_cast<uint8_t>(engineIntakeManifold3TemperatureC + 40);  // 1132
+#endif
+#if defined(SPN_1133_INTAKE_MANIFOLD_4_AIR_TEMPERATURE)
   msg.buf[2] =
       static_cast<uint8_t>(engineIntakeManifold4TemperatureC + 40);  // 1133
+#endif
   msg.buf[3] = 255;
   msg.buf[4] = 255;
   msg.buf[5] = 255;
@@ -128,10 +142,14 @@ void J1939Bus::sendPgn65190(float engineTurbocharger1BoostPressurekPa,
   msg.id = message.canId;
   float boost1Offset = engineTurbocharger1BoostPressurekPa / 0.125;
   float boost2Offset = engineTurbocharger2BoostPressurekPa / 0.125;
+#if defined(SPN_1127_TURBOCHARGER_1_BOOST_PRESSURE)
   msg.buf[0] = highByte(static_cast<uint16_t>(boost1Offset));  // 1127
   msg.buf[1] = lowByte(static_cast<uint16_t>(boost1Offset));   // 1127
+#endif
+#if defined(SPN_1128_TURBOCHARGER_2_BOOST_PRESSURE)
   msg.buf[2] = highByte(static_cast<uint16_t>(boost2Offset));  // 1128
   msg.buf[3] = lowByte(static_cast<uint16_t>(boost2Offset));   // 1128
+#endif
   msg.buf[4] = 255;
   msg.buf[5] = 255;
   msg.buf[6] = 255;
@@ -153,10 +171,16 @@ void J1939Bus::sendPgn65262(float engineCoolantTemperatureC,
   msg.id = message.canId;
   float oilTempOffset = engineOilTemperatureC + 273.0;
   oilTempOffset /= 0.03125;
+#if defined(SPN_110_ENGINE_COOLANT_TEMPERATURE)
   msg.buf[0] = static_cast<uint8_t>(engineCoolantTemperatureC + 40);  // 110
+#endif
+#if defined(SPN_174_FUEL_TEMPERATURE)
   msg.buf[1] = static_cast<uint8_t>(engineFuelTemperatureC + 40);     // 174
+#endif
+#if defined(SPN_175_ENGINE_OIL_TEMPERATURE)
   msg.buf[2] = highByte(static_cast<uint16_t>(oilTempOffset));        // 175
   msg.buf[3] = lowByte(static_cast<uint16_t>(oilTempOffset));         // 175
+#endif
   msg.buf[4] = 255;
   msg.buf[5] = 255;
   msg.buf[6] = 255;
@@ -176,13 +200,19 @@ void J1939Bus::sendPgn65263(float engineFuelDeliveryPressurekPa,
   CAN_message_t msg;
   msg.flags.extended = 1;
   msg.id = message.canId;
+#if defined(SPN_94_FUEL_DELIVERY_PRESSURE)
   msg.buf[0] = static_cast<uint8_t>(engineFuelDeliveryPressurekPa / 4);  // 94
+#endif
   msg.buf[1] = 255;
   msg.buf[2] = 255;
+#if defined(SPN_100_ENGINE_OIL_PRESSURE)
   msg.buf[3] = static_cast<uint8_t>(engineOilPressurekPa / 4);  // 100
+#endif
   msg.buf[4] = 255;
   msg.buf[5] = 255;
+#if defined(SPN_109_COOLANT_PRESSURE)
   msg.buf[6] = static_cast<uint8_t>(engineCoolantPressurekPa / 2);  // 109
+#endif
   msg.buf[7] = 255;
 
   Can1.write(msg);
@@ -201,12 +231,18 @@ void J1939Bus::sendPgn65269(float ambientTemperatureC,
   msg.id = message.canId;
   float ambientAirTempOffset = ambientTemperatureC + 273.0;
   ambientAirTempOffset /= 0.03125;
+#if defined(SPN_108_BAROMETRIC_PRESSURE)
   msg.buf[0] = static_cast<uint8_t>(barometricPressurekPa * 2);  // 108
+#endif
   msg.buf[1] = 255;
   msg.buf[2] = 255;
+#if defined(SPN_171_AMBIENT_AIR_TEMP)
   msg.buf[3] = highByte(static_cast<uint16_t>(ambientAirTempOffset));  // 171
   msg.buf[4] = lowByte(static_cast<uint16_t>(ambientAirTempOffset));   // 171
+#endif
+#if defined(SPN_172_AIR_INLET_TEMPERATURE)
   msg.buf[5] = static_cast<uint8_t>(airInletTemperatureC + 40);        // 172
+#endif
   msg.buf[6] = 255;
   msg.buf[7] = 255;
 
@@ -227,12 +263,20 @@ void J1939Bus::sendPgn65270(float airInletPressurekPa,
   float egtOffset = egtTemperatureC + 273.0;
   egtOffset /= 0.03125;
   msg.buf[0] = 255;
+#if defined(SPN_102_BOOST_PRESSURE)
   msg.buf[1] = static_cast<uint8_t>(boostPressurekPa / 2);       // 102
+#endif
+#if defined(SPN_1363_INTAKE_MANIFOLD_1_AIR_TEMPERATURE)
   msg.buf[2] = static_cast<uint8_t>(airInletTemperatureC + 40);  // 105
+#endif
+#if defined(SPN_106_AIR_INLET_PRESSURE)
   msg.buf[3] = static_cast<uint8_t>(airInletPressurekPa / 2);    // 106
+#endif
   msg.buf[4] = 255;
+#if defined(SPN_173_EXHAUST_GAS_TEMPERATURE)
   msg.buf[5] = highByte(static_cast<uint16_t>(egtOffset));  // 173
   msg.buf[6] = lowByte(static_cast<uint16_t>(egtOffset));   // 173
+#endif
   msg.buf[7] = 255;
   Can1.write(msg);
 }
--- src/Domain/ossm.cpp
+++ src/Domain/ossm.cpp
@@ -1,17 +1,19 @@
 #include "ossm.h"
+#include "configuration.h"
 
+#if defined(SPN_173_EXHAUST_GAS_TEMPERATURE)
 #include <Adafruit_MAX31855.h>
+#endif
 #include <AppData.h>
 #include <Arduino.h>
+#if defined(SPN_102_BOOST_PRESSURE) || defined(SPN_106_AIR_INLET_PRESSURE) || defined(SPN_109_COOLANT_PRESSURE) || defined(SPN_94_FUEL_DELIVERY_PRESSURE) || defined(SPN_1127_TURBOCHARGER_1_BOOST_PRESSURE) || defined(SPN_1128_TURBOCHARGER_2_BOOST_PRESSURE)
 #include <PressureSensor.h>
+#endif
+#if defined(SPN_172_AIR_INLET_TEMPERATURE) || defined(SPN_105_INTAKE_MANIFOLD_1_TEMPERATURE) || defined(SPN_175_ENGINE_OIL_TEMPERATURE) || defined(SPN_110_ENGINE_COOLANT_TEMPERATURE) || defined(SPN_174_FUEL_TEMPERATURE) || defined(SPN_1637_ENGINE_COOLANT_TEMPERATURE) || defined(SPN_1363_INTAKE_MANIFOLD_1_AIR_TEMPERATURE) || defined(SPN_1131_INTAKE_MANIFOLD_2_AIR_TEMPERATURE) || defined(SPN_1132_INTAKE_MANIFOLD_3_AIR_TEMPERATURE) || defined(SPN_1133_INTAKE_MANIFOLD_4_AIR_TEMPERATURE) || defined(SPN_441_ENGINE_BAY_TEMPERATURE)
 #include <TempSensor.h>
+#endif
 
 #include "Display/J1939Bus.h"
-
-#define AEM_TEMP_SENSOR_A 1.485995686e-03
-#define AEM_TEMP_SENSOR_B 2.279654266e-04
-#define AEM_TEMP_SENSOR_C 1.197578033e-07
-#define MAXCS 10
 
 bool ossm::isAds1Initialized = false;
 bool ossm::isAds2Initialized = false;
@@ -21,23 +23,57 @@ bool ossm::isEgtInitialized = false;
 bool ossm::isBmeInitialized = false;
 
 AppData ossm::appData;
+#if defined(SPN_171_AMBIENT_AIR_TEMP) || defined(SPN_108_BAROMETRIC_PRESSURE) || defined(SPN_354_RELATIVE_HUMIDITY)
 AmbientSensors *ossm::ambientSensors;
+#endif
+#if defined(SPN_175_ENGINE_OIL_TEMPERATURE)
 TempSensor oilTempSensor;
+#endif
+#if defined(SPN_100_ENGINE_OIL_PRESSURE)
 PressureSensor oilPressureSensor;
+#endif
+#if defined(SPN_110_ENGINE_COOLANT_TEMPERATURE)
 TempSensor coolantTempSensor;
+#endif
+#if defined(SPN_109_COOLANT_PRESSURE)
 PressureSensor coolantPressureSensor;
+#endif
+#if defined(SPN_1132_INTAKE_MANIFOLD_3_AIR_TEMPERATURE)
 TempSensor tranferPipeTempSensor;
+#endif
+#if defined(SPN_1128_TURBOCHARGER_2_BOOST_PRESSURE)
 PressureSensor transferPipePressureSensor;
+#endif
+#if defined(SPN_105_INTAKE_MANIFOLD_1_TEMPERATURE)
 TempSensor boostTempSensor;
+#endif
+#if defined(SPN_102_BOOST_PRESSURE)
 PressureSensor boostPressureSensor;
+#endif
+#if defined(SPN_1127_TURBOCHARGER_1_BOOST_PRESSURE)
 PressureSensor cacPressureSensor;
+#endif
+#if defined(SPN_1131_INTAKE_MANIFOLD_2_AIR_TEMPERATURE)
 TempSensor cacTempSensor;
+#endif
+#if defined(SPN_106_AIR_INLET_PRESSURE)
 PressureSensor intakePressureSensor;
+#endif
+#if defined(SPN_172_AIR_INLET_TEMPERATURE)
 TempSensor intakeTempSensor;
+#endif
+#if defined(SPN_94_FUEL_DELIVERY_PRESSURE)
 PressureSensor fuelPressureSensor;
+#endif
+#if defined(SPN_174_FUEL_TEMPERATURE)
 TempSensor fuelTempSensor;
+#endif
+#if defined(SPN_441_ENGINE_BAY_TEMPERATURE)
 TempSensor engineBayTempSensor;
+#endif
+#if defined(SPN_173_EXHAUST_GAS_TEMPERATURE)
 Adafruit_MAX31855 thermocouple(MAXCS);
+#endif
 
 uint32_t lastOneSecondMillis;
 uint32_t lastHalfSecondMillis;
@@ -51,111 +87,179 @@ void ossm::setup() {
 
   J1939Bus::initialize(&ossm::appData);
 
+#if defined(SPN_171_AMBIENT_AIR_TEMP) || defined(SPN_108_BAROMETRIC_PRESSURE) || defined(SPN_354_RELATIVE_HUMIDITY)
   ossm::ambientSensors = new AmbientSensors(&ossm::isBmeInitialized);
-
+#endif
+#if defined(SPN_175_ENGINE_OIL_TEMPERATURE)
   oilTempSensor =
       TempSensor(10000, AEM_TEMP_SENSOR_A, AEM_TEMP_SENSOR_B, AEM_TEMP_SENSOR_C,
                  0x48, 0, 0, &ossm::isAds1Initialized);
+#endif
+#if defined(SPN_100_ENGINE_OIL_PRESSURE)
   oilPressureSensor = PressureSensor(0x48, 1, 150);
+#endif
+#if defined(SPN_110_ENGINE_COOLANT_TEMPERATURE)
   coolantTempSensor =
       TempSensor(10000, AEM_TEMP_SENSOR_A, AEM_TEMP_SENSOR_B, AEM_TEMP_SENSOR_C,
                  0x48, 2, 0, &ossm::isAds1Initialized);
+#endif
+#if defined(SPN_109_COOLANT_PRESSURE)
   coolantPressureSensor = PressureSensor(0x48, 3, 50);
+#endif
 
+#if defined(SPN_1132_INTAKE_MANIFOLD_3_AIR_TEMPERATURE)
   tranferPipeTempSensor =
       TempSensor(10000, AEM_TEMP_SENSOR_A, AEM_TEMP_SENSOR_B, AEM_TEMP_SENSOR_C,
                  0x49, 0, 0, &ossm::isAds2Initialized);
+#endif
+#if defined(SPN_1128_TURBOCHARGER_2_BOOST_PRESSURE)
   transferPipePressureSensor = PressureSensor(0x49, 1, 58);
+#endif
+#if defined(SPN_1363_INTAKE_MANIFOLD_1_AIR_TEMPERATURE)
   boostTempSensor =
       TempSensor(10000, AEM_TEMP_SENSOR_A, AEM_TEMP_SENSOR_B, AEM_TEMP_SENSOR_C,
                  0x49, 3, 0, &ossm::isAds2Initialized);
+#endif
+#if defined(SPN_102_BOOST_PRESSURE)
   boostPressureSensor = PressureSensor(0x49, 2, 150);
+#endif
 
+#if defined(SPN_1131_INTAKE_MANIFOLD_2_AIR_TEMPERATURE)
   cacTempSensor =
       TempSensor(10000, AEM_TEMP_SENSOR_A, AEM_TEMP_SENSOR_B, AEM_TEMP_SENSOR_C,
                  0x4A, 1, 0, &ossm::isAds3Initialized);
+#endif
+#if defined(SPN_1127_TURBOCHARGER_1_BOOST_PRESSURE)
   cacPressureSensor = PressureSensor(0x4A, 0, 150);
+#endif
+#if defined(SPN_172_AIR_INLET_TEMPERATURE)
   intakeTempSensor =
       TempSensor(10000, AEM_TEMP_SENSOR_A, AEM_TEMP_SENSOR_B, AEM_TEMP_SENSOR_C,
                  0x4A, 2, 0, &ossm::isAds3Initialized);
+#endif
+#if defined(SPN_102_BOOST_PRESSURE)
   intakePressureSensor = PressureSensor(0x4A, 3, 30);
+#endif
 
+#if defined(SPN_174_FUEL_TEMPERATURE)
   fuelTempSensor =
       TempSensor(10000, AEM_TEMP_SENSOR_A, AEM_TEMP_SENSOR_B, AEM_TEMP_SENSOR_C,
                  0x4B, 1, 0, &ossm::isAds4Initialized);
-  fuelPressureSensor = PressureSensor(0x4B, 0, 150);
+#endif
+#if defined(SPN_94_FUEL_DELIVERY_PRESSURE)
+  fuelPressureSensor = PressureSensor(0x4B, 0, 100);
+#endif
+#if defined(SPN_441_ENGINE_BAY_TEMPERATURE)
   engineBayTempSensor =
       TempSensor(10000, AEM_TEMP_SENSOR_A, AEM_TEMP_SENSOR_B, AEM_TEMP_SENSOR_C,
                  0x4B, 2, 0, &ossm::isAds4Initialized);
+#endif
 
+#if defined(SPN_173_EXHAUST_GAS_TEMPERATURE)
   if (!thermocouple.begin()) {
     Serial.println("ERROR INITING THERMOCOUPLE");
   } else {
     ossm::isEgtInitialized = true;
     Serial.println("Thermocouple initialized");
   }
+#endif
 
+#if defined(SPN_175_ENGINE_OIL_TEMPERATURE) || defined(SPN_100_ENGINE_OIL_PRESSURE) || defined(SPN_110_ENGINE_COOLANT_TEMPERATURE) || defined(SPN_109_COOLANT_PRESSURE)
   Serial.println("isAds1Initialized->" + String(ossm::isAds1Initialized));
+#endif
+#if defined(SPN_102_BOOST_PRESSURE) || defined(SPN_1128_TURBOCHARGER_2_BOOST_PRESSURE) || defined(SPN_1132_INTAKE_MANIFOLD_3_AIR_TEMPERATURE) || defined(SPN_1363_INTAKE_MANIFOLD_1_AIR_TEMPERATURE)
   Serial.println("isAds2Initialized->" + String(ossm::isAds2Initialized));
+#endif
+#if defined(SPN_172_AIR_INLET_TEMPERATURE) || defined(SPN_1127_TURBOCHARGER_1_BOOST_PRESSURE) || defined(SPN_1131_INTAKE_MANIFOLD_2_AIR_TEMPERATURE) || defined(SPN_106_AIR_INLET_PRESSURE) || defined(SPN_1133_INTAKE_MANIFOLD_4_AIR_TEMPERATURE)
   Serial.println("isAds3Initialized->" + String(ossm::isAds3Initialized));
+#endif
+#if defined(SPN_94_FUEL_DELIVERY_PRESSURE) || defined(SPN_174_FUEL_TEMPERATURE) || defined(SPN_441_ENGINE_BAY_TEMPERATURE)
   Serial.println("isAds4Initialized->" + String(ossm::isAds4Initialized));
+#endif
   Serial.println("isEgtInitialized->" + String(ossm::isEgtInitialized));
+#if defined(SPN_171_AMBIENT_AIR_TEMP) || defined(SPN_108_BAROMETRIC_PRESSURE)
   Serial.println("isBmeInitialized->" + String(ossm::isBmeInitialized));
+#endif
 }
 
 void ossm::loop() {
   thisMillis = millis();
 
+#if defined(SPN_171_AMBIENT_AIR_TEMP) || defined(SPN_108_BAROMETRIC_PRESSURE) || defined(SPN_354_RELATIVE_HUMIDITY)
   if (ossm::isBmeInitialized == true) {
     ossm::appData.absoluteBarometricpressurekPa =
         ossm::ambientSensors->getPressurekPa();
     ossm::appData.humidity = ossm::ambientSensors->getHumidity();
     ossm::appData.ambientTemperatureC = ossm::ambientSensors->getTemperatureC();
   }
+#endif
 
+#if defined(SPN_175_ENGINE_OIL_TEMPERATURE) || defined(SPN_100_ENGINE_OIL_PRESSURE) || defined(SPN_110_ENGINE_COOLANT_TEMPERATURE) || defined(SPN_109_COOLANT_PRESSURE)
   if (ossm::isAds1Initialized) {
     ossm::appData.oilTemperatureC = oilTempSensor.getTempCelsius();
     ossm::appData.oilPressurekPa = oilPressureSensor.getPressureInkPa();
     ossm::appData.coolantTemperatureC = coolantTempSensor.getTempCelsius();
     ossm::appData.coolantPressurekPa = coolantPressureSensor.getPressureInkPa();
   }
-
+#endif
+#if defined(SPN_102_BOOST_PRESSURE) || defined(SPN_1128_TURBOCHARGER_2_BOOST_PRESSURE) || defined(SPN_1132_INTAKE_MANIFOLD_3_AIR_TEMPERATURE) || defined(SPN_1363_INTAKE_MANIFOLD_1_AIR_TEMPERATURE)
   if (ossm::isAds2Initialized == true) {
     ossm::appData.tranferPipePressurekPa =
         transferPipePressureSensor.getPressureInkPa();
     ossm::appData.transferPipeTemperatureC =
         tranferPipeTempSensor.getTempCelsius();
+#if defined(SPN_102_BOOST_PRESSURE)
     ossm::appData.boostPressurekPa = boostPressureSensor.getPressureInkPa();
+#endif
     ossm::appData.boostTemperatureC = boostTempSensor.getTempCelsius();
   }
+#endif
 
+#if defined(SPN_172_AIR_INLET_TEMPERATURE) || defined(SPN_1127_TURBOCHARGER_1_BOOST_PRESSURE) || defined(SPN_1131_INTAKE_MANIFOLD_2_AIR_TEMPERATURE) || defined(SPN_106_AIR_INLET_PRESSURE) || defined(SPN_1133_INTAKE_MANIFOLD_4_AIR_TEMPERATURE)
   if (ossm::isAds3Initialized == true) {
     ossm::appData.cacInletPressurekPa = cacPressureSensor.getPressureInkPa();
     ossm::appData.cacInletTemperatureC = cacTempSensor.getTempCelsius();
     ossm::appData.airInletPressurekPa = intakePressureSensor.getPressureInkPa();
     ossm::appData.airInletTemperatureC = intakeTempSensor.getTempCelsius();
   }
+#endif
 
+#if defined(SPN_94_FUEL_DELIVERY_PRESSURE) || defined(SPN_174_FUEL_TEMPERATURE) || defined(SPN_441_ENGINE_BAY_TEMPERATURE)
   if (ossm::isAds4Initialized == true) {
+#if defined(SPN_94_FUEL_DELIVERY_PRESSURE)
     ossm::appData.fuelPressurekPa = fuelPressureSensor.getPressureInkPa();
+#endif
+#if defined(SPN_174_FUEL_TEMPERATURE)
     ossm::appData.fuelTemperatureC = fuelTempSensor.getTempCelsius();
+#endif
+#if defined(SPN_441_ENGINE_BAY_TEMPERATURE)
     ossm::appData.engineBayTemperatureC = engineBayTempSensor.getTempCelsius();
+#endif
   }
+#endif
 
+#if defined(SPN_173_EXHAUST_GAS_TEMPERATURE)
   if (ossm::isEgtInitialized) {
     ossm::appData.egtTemperatureC = thermocouple.readCelsius();
   }
+#endif
 
   // Every .5 seconds
   if (lastHalfSecondMillis - thisMillis >= 500) {
+#if defined(SPN_173_EXHAUST_GAS_TEMPERATURE) || defined(SPN_102_BOOST_PRESSURE) || defined(SPN_106_AIR_INLET_PRESSURE) || defined(SPN_1363_INTAKE_MANIFOLD_1_AIR_TEMPERATURE)
     J1939Bus::sendPgn65270(
         ossm::appData.airInletPressurekPa, ossm::appData.boostTemperatureC,
         ossm::appData.egtTemperatureC, ossm::appData.boostPressurekPa);
+#endif
+#if defined(SPN_94_FUEL_DELIVERY_PRESSURE) || defined(SPN_100_ENGINE_OIL_PRESSURE) || defined(SPN_109_COOLANT_PRESSURE)
     J1939Bus::sendPgn65263(ossm::appData.fuelPressurekPa,
                            ossm::appData.oilPressurekPa,
                            ossm::appData.coolantPressurekPa);
+#endif
+#if defined(SPN_1127_TURBOCHARGER_1_BOOST_PRESSURE) || defined(SPN_1128_TURBOCHARGER_2_BOOST_PRESSURE)
     J1939Bus::sendPgn65190(ossm::appData.cacInletPressurekPa,
                            ossm::appData.tranferPipePressurekPa);
+#endif
     lastHalfSecondMillis = thisMillis;
   }
 
@@ -203,23 +307,32 @@ void ossm::loop() {
       // Serial.println(
       //     "AppData: Fuel Pressure->" + String(ossm::appData.fuelPressurekPa)
       //     + "kPa, Fuel Temperature->" +
-      //     String(ossm::appData.fuelTemperatureC) + "°C, Engine Bay
-      //     Temperature->" + String(ossm::appData.engineBayTemperatureC) +
+      //     String(ossm::appData.fuelTemperatureC) + "°C, Engine Bay Temperature->" + String(ossm::appData.engineBayTemperatureC) +
       //     "°C");
     }
-
+#if defined(SPN_173_EXHAUST_GAS_TEMPERATURE)
+    // Serial.println("EGT??? " + (String)ossm::isEgtInitialized);
+    // Serial.println("EGT? " + (String)ossm::appData.egtTemperatureC);
+#endif
+#if defined(SPN_171_AMBIENT_AIR_TEMP) || defined(SPN_172_AIR_INLET_TEMPERATURE) || defined(SPN_108_BAROMETRIC_PRESSURE)
     J1939Bus::sendPgn65269(ossm::appData.ambientTemperatureC,
                            ossm::appData.airInletTemperatureC,
                            ossm::appData.absoluteBarometricpressurekPa);
-
+#endif                           
+#if defined(SPN_1363_INTAKE_MANIFOLD_1_AIR_TEMPERATURE) || defined(SPN_110_ENGINE_COOLANT_TEMPERATURE)
     J1939Bus::sendPgn65129(ossm::appData.boostTemperatureC,
                            ossm::appData.coolantTemperatureC);
+#endif
+#if defined(SPN_110_ENGINE_COOLANT_TEMPERATURE) || defined(SPN_174_FUEL_TEMPERATURE) || defined(SPN_175_ENGINE_OIL_TEMPERATURE)
     J1939Bus::sendPgn65262(ossm::appData.coolantTemperatureC,
                            ossm::appData.fuelTemperatureC,
                            ossm::appData.oilTemperatureC);
+#endif
+#if defined(SPN_1131_INTAKE_MANIFOLD_2_AIR_TEMPERATURE) || defined(SPN_1132_INTAKE_MANIFOLD_3_AIR_TEMPERATURE) || defined(SPN_1133_INTAKE_MANIFOLD_4_AIR_TEMPERATURE)
     J1939Bus::sendPgn65189(ossm::appData.cacInletTemperatureC,
                            ossm::appData.transferPipeTemperatureC,
                            ossm::appData.airInletTemperatureC);
+#endif
 
     lastOneSecondMillis = thisMillis;
   }
